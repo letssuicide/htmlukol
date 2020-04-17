7d87440025841c9f39fc6122db6acb7228b949f1
janho <janho@192.168.0.17>
2020-04-17 11:31:38 +0200

    ..

---
+++ test.php
@@ -0,0 +1,90 @@
+
+
+<?php
+echo 19;
+echo"<br>";
+print (1.1);
+echo"<br>";
+echo "zadek";
+echo"<br>";
+print(TRUE);
+echo"čau "."zdravim ";
+echo"<img src\"rouška3.jpg\">";
+echo"\\";
+
+$a = 9;
+            echo"<br>";
+            $b = 7;
+            echo "<br>";
+            var_dump($a + $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a - $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a * $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a / $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a += $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a -= $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a *= $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a /= $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+            var_dump ($a == $b);
+            echo "<br>";
+            $a = 90;
+            echo"<br>";
+            $b = 7;
+
+            var_dump ($a != $b);
+            echo "<br>";
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+            var_dump ($a > $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+            var_dump ($a < $b);
+
+            echo "<br>";
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+            var_dump ($a || $b);
+
+            echo "<br>";
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+            var_dump ($a & $b);
+?>

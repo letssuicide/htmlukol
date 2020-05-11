2e35aa893e028ff50e4365ed54b389e804267e12
janho <janho@192.168.0.17>
2020-05-11 11:02:59 +0200

    ..

--- indexphp/index.php
+++ indexphp/index.php
@@ -149,6 +149,53 @@ $a = 9;
                                  );
                            dump($produkty);  
                            
+   $radekhostalekjan=5; 
+$sloupechostalekjan=16; 
+            echo "<table border=1>";
+            for($i=1;$i<=$radekhostalekjan;$i++) {
+                echo "<tr>";
+for($x=1;$x<=$sloupechostalekjan;$x++){
+    echo "<td>".$i."-".$x."</td>";
+}
+                echo "</tr>";
+            }
+            echo "</table><br><br><br>";
+
+           echo abs(6)."<br>";
+            echo ceil(150)."<br>";
+           $string="whats up";
+          echo strstr($string, "up")."<br>";
+          echo strpos($string, "up")."<br>";
+          echo sizeof($produkty);
+          array_splice($produkty, 0, 1, "oi1"); 
+          dump($produkty);
+
+          $i=1;
+          foreach ($produkty as $produkt){
+              echo $i."-".$produkt."<br>";
+              $i++;
+          }
+
+                  $vyska="180cm";        
+        echo "velikost: ".$vyska;
+        dump(velikost($vyska)); 
+      
+        function velikost (string $vyska, bool $volba = false){       
+            $rust=substr($vyska,0,2);
+            $kilogram= substr(date("Y"),2,2);
+            if($rust <=$kilogram){ 
+                $rust= "20".$rust;
+            }
+            else{
+                $narozeni= "19".$narozeni;
+            }
+            $vek=date("Y")-$narozeni;
+            if ($volba == true){
+                return $narozeni;
+            }
+            return $vek;
+        }
+                           
 
                            
        
